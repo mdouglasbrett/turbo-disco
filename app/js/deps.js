6a0f34c11858c053c8a19b7f4954ef76ce5128c6
mdouglasbrett <mdouglasbrett@icloud.com>
2021-03-28 00:19:05 +0000

    wip: add indexedDB wrapper

--- app/js/deps.js
+++ app/js/deps.js
@@ -1,4 +1,5 @@
 import { html, LitElement } from "https://cdn.skypack.dev/lit-element";
+import Dexie from 'https://cdn.skypack.dev/dexie';
 import reduxJsToolkit from "https://cdn.skypack.dev/@reduxjs/toolkit";
 
-export { html, LitElement, reduxJsToolkit };
+export { html, LitElement, reduxJsToolkit, Dexie };
