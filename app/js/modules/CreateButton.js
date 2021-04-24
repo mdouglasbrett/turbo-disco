fc0f32c545c188d4194cfb3cac7cf904850788cd
Michael Brett <mdouglasbrett@icloud.com>
2021-04-24 16:58:18 +0100

    chore: fomatting

--- app/js/modules/CreateButton.js
+++ app/js/modules/CreateButton.js
@@ -1,9 +1,9 @@
 import { html, LitElement } from "../deps.js";
 
 export class CreateButton extends LitElement {
-    _handleClick() {
-        return console.log("clicked the button");
-    }
+  _handleClick() {
+    return console.log("clicked the button");
+  }
   render() {
     return html`<button @click=${this._handleClick}>Create</button>`;
   }
--- app/js/modules/ToDoList.js
+++ app/js/modules/ToDoList.js
@@ -1,26 +1,29 @@
 import { html, LitElement } from "../deps.js";
 
 export class ToDoList extends LitElement {
-    _handleChange() {
-        return console.log('changed input state');
-    }
+  _handleChange() {
+    return console.log("changed input state");
+  }
 
   render() {
-    return (
-      html`<ul>
-              <li>
-                <label for="checkbox-1">
-                  <input @change=${this._handleChange} type="checkbox" name="checkbox-1" id="checkbox-1"/>
-                  Item One
-                </label>
-              </li>
-              <li>
-                <label for="checkbox-2">
-                  <input type="checkbox" name="checkbox-2" id="checkbox-2" />
-                Item Two
-                </label>
-              </li>
-            </ul>`
-    );
+    return html`<ul>
+      <li>
+        <label for="checkbox-1">
+          <input
+            @change=${this._handleChange}
+            type="checkbox"
+            name="checkbox-1"
+            id="checkbox-1"
+          />
+          Item One
+        </label>
+      </li>
+      <li>
+        <label for="checkbox-2">
+          <input type="checkbox" name="checkbox-2" id="checkbox-2" />
+          Item Two
+        </label>
+      </li>
+    </ul>`;
   }
 }
