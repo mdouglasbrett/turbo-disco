a0d594a017f7a2d11675093ea69021f6c57ec3e7
Michael Brett <mdouglasbrett@icloud.com>
2021-07-17 17:00:45 +0100

    wip: initial worker steps

---
+++ app/js/db.js
@@ -0,0 +1,12 @@
+self.addEventListener("message", handleWorkerMessage)
+
+function handleWorkerMessage(message) {
+    var { data } = message;
+    switch (data.type) {
+        case "createToDo":
+            self.postMessage({ command: "createToDo", payload: `Here's what you want me to create: ${data.payload}`})
+            break;
+        default:
+            break
+    }
+}
